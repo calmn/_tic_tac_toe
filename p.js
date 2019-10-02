9fa67686dae546947de4d5950acb23b28a2c0de5
calmn <abraham.valdomero@gmail.com>
2019-10-01 20:34:38 -0500

    Agregando algunas restricciones

--- p.js
+++ p.js
@@ -13,45 +13,91 @@ btnCruz.addEventListener('click', () => {
 })
 var btnInicia = document.querySelector("#btnInicia");
 btnInicia.addEventListener('click', () => {
-    console.log(ficha_elegida);
     document.querySelector("#opciones").style.display = 'none';
     document.querySelector("#panel-juego").style.display = 'block';
+    elegirJugadorInicia();
 })
-
 /* Aquí */
 const tablero = [
     0,0,0,
     0,0,0,
     0,0,0
 ]
+const movsGanadores = [
+    {pini : 0, pmed: 1, pfin: 2},
+    {pini : 0, pmed: 4, pfin: 8},
+    {pini : 0, pmed: 3, pfin: 6},        
+    {pini : 1, pmed: 4, pfin: 7},
+    {pini : 2, pmed: 4, pfin: 6},
+    {pini : 2, pmed: 5, pfin: 8},
+    {pini : 3, pmed: 4, pfin: 5},
+    {pini : 6, pmed: 7, pfin: 8},
+]
 const ficha_jug = 1;
 const ficha_ia = 2;
 var btnsTablero = document.querySelectorAll("#tablero button");
-btnsTablero.forEach((boton,idx) => {
-    boton.addEventListener('click',() => {
-        boton.style.backgroundImage = `url('img/${ficha_elegida}.png')`;
-        console.log(idx);
-        tablero[idx] = ficha_jug;
-        validarVictoria(ficha_jug);
+function imprimirMensaje(mensaje) {
+    var parrafo = document.querySelector("#msj-inicia");
+    parrafo.textContent = mensaje;
+}
+function elegirJugadorInicia() { 
+    console.log(ficha_elegida);
+    inicializarEventoBotones();   
+    if(Math.floor(Math.random() * 2) == 0) {
+        imprimirMensaje("Es tu turno");
+    } else {
+        imprimirMensaje("Buscando mejor tiro bi bop bi bop...");
+        buscarMejorPosicion();
+    }
+}
+function fichasElegidas(arreglo) {
+    var arre = arreglo.reduce((arre, reg, idx) => {
+        if(reg != 0) arre.push(idx);
+        return arre;
+    },[])
+    return (arre === undefined) ? 0 : arre.length;
+}
+function posicionesDisponibles(arreglo) {
+    /* Retorna un arreglo con todas las posiciones libres */
+    var arre = arreglo.reduce( (arre,reg,idx) => {
+        if(reg === 0) arre.push(idx);
+        return arre;
+    },[])
+    return arre;
+}
+function inicializarEventoBotones() {
+    btnsTablero.forEach((boton,idx) => {
+        boton.addEventListener('click',() => {
+            if(fichasElegidas(tablero) < 9) {
+                boton.style.backgroundImage = `url('img/${ficha_elegida}.png')`;
+                tablero[idx] = ficha_jug;
+                validarVictoria(ficha_jug);
+                btnsTablero.forEach(btn => btn.disabled = true)
+                setTimeout(buscarMejorPosicion,1000); 
+            }      
+        })
     })
-})
+}
 function validarVictoria(ficha_id) {
-    const movsGanadores = [
-        {pini : 0, pmed: 1, pfin: 2},
-        {pini : 0, pmed: 4, pfin: 8},
-        {pini : 0, pmed: 3, pfin: 6},        
-        {pini : 1, pmed: 4, pfin: 7},
-        {pini : 2, pmed: 4, pfin: 6},
-        {pini : 2, pmed: 5, pfin: 8},
-        {pini : 3, pmed: 4, pfin: 5},
-        {pini : 6, pmed: 7, pfin: 8},
-    ]
     movsGanadores.forEach(fila=>{
-        if(
-            tablero[fila.pini] == ficha_id && tablero[fila.pmed] == ficha_id && tablero[fila.pfin] == ficha_id
-        ) 
+        if(tablero[fila.pini] == ficha_id && tablero[fila.pmed] == ficha_id && tablero[fila.pfin] == ficha_id) 
         {
-            console.log("victoria" + ficha_id);
+            console.log("victoria de: " + ficha_id);
         }
+    })
+}
+function buscarMejorPosicion() {
+    if(fichasElegidas(tablero) < 9) {
+        var arreglo = posicionesDisponibles(tablero);
+        var x = Math.floor(Math.random() * arreglo.length);
+        tablero[arreglo[x]] = ficha_ia;   
+        btnsTablero[arreglo[x]].style.backgroundImage = `url('img/${(ficha_elegida === 'circulo') ? 'cruz' : 'circulo'}.png')`;
+        validarVictoria(ficha_ia);
+        activarBotonesDisponibles(tablero)
+    }    
+}
+function activarBotonesDisponibles(tablero) {
+    tablero.forEach((pos,idx)=>{
+        if(pos == 0)   btnsTablero[idx].disabled = false;
     })
 }
